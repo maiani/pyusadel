78efebcb912760ba9f539c255cf5931789f986a2
Andrea Maiani <andrea.maiani@nbi.ku.dk>
2022-09-05 14:00:04 +0200

    Added fitting routines (wip)

---
+++ pyusadel/model_fitting.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pyusadel import (
+    DifferentialOperators,
+    UsadelProblem,
+)
+
+
+class ModelFitting:
+    def __init__(self):
+        pass
